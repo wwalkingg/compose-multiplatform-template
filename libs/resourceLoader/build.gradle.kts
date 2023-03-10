69719d00bb6e71c89a261bd6491eb1bee7bef198
wwalkingg <wwalkingg@outlook.com>
2023-03-10 21:08:35 +0800

    change compile java version to 17

--- libs/resourceLoader/build.gradle.kts
+++ libs/resourceLoader/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         compileSdk = 33
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
